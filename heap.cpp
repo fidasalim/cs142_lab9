84955bddccf5f77691d56c82c143405bc7c1ded5
fidasalim <42141530+fidasalim@users.noreply.github.com>
2019-04-19 07:28:26 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,171 +1,120 @@
 #include<iostream>
 using namespace std;
-
-/*
-
-2 Rules for Binary Heap
-- Its a complete tree. -> So you can use an array
-- Either a max-heap or min-heap. 
-
-Array Implementation 
-Root - A[0]
-
-Parent Node : Arr[(i-1)/2]
-Left Child  : Arr[(2*i)+1]
-Right Child : Arr[(2*i)+2]
-
-Functions:
-1. GetMin()
-2. ExtractMin()
-3. DecreaseKey()
-4. Insert()
-5. Delete()
-6. Display()
-
-*/
-
-
+//class for heap
 class BinaryHeap{
     public:
-    // Pointer to array of elements
-    int * hArr;
-    // Maximum possible size of array
-    int mSize;
-    // Current size of array 
-    int cSize;
-
-    BinaryHeap(int mSize){
-        // Constructor 
-        hArr = new int[mSize];
-        this->mSize = mSize;
-        cSize = 0;
-    }
-
-    void swap(int *a, int *b){
-        // Swap elements ... 
-        int temp = *a; 
-        *a = *b; 
-        *b = temp;
-    }
-
-    int parent(int i){ return (i-1)/2;}
-    int left(int i){ return (i*2)+1;}
-    int right(int i){ return (i*2)+2;}
-
-    int getMin(){ return hArr[0]; }
-
-    void insert(int value){
-        // Base case. for Heap Overflow
-	if(cSize == mSize){
-		cout<<"Can't insert more elements\n";
+	//pointer to the array
+	int *hArr;
+	//maximum size
+	int mSize;
+	//current size
+	int cSize;
+	
+	//**************************************************************************
+	
+	//constructer
+	BinaryHeap(int mSize){
+		hArr = new int[mSize];
+		this->mSize = mSize;
+		cSize = 0;
 	}
-        // Insert the new key at the end.
-        else{
-        	hArr[cSize] = value;	
-		//Check it for all hierarchy with loop
-		int i = cSize;
-			//Check if it breaks the heap property and fix it. 
-		while (i != 0 && hArr[parent(i)]>hArr[i]){
-			swap(&hArr[parent(i)],&hArr[i]);
-			i = parent(i);
+	//**************************************************************************
+	void swap(int *a,int *b){
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+	//**************************************************************************
+	int left(int i){ return (i*2)+1;}
+	int right(int i){ return (i*2)+2;}
+	int parent(int i){ return (i-1)/2;}
+	
+	int get_min(){return hArr[0];}
+	//**************************************************************************
+	void insert(int value){
+		//when overflowed
+		if(cSize == mSize){
+			cout<<"noT possible"<<endl;
+			return;
 		}
-		cSize++;
-	}
-    }
-    void display(){
-        // Display the array
-        cout<<"The array you made is :\n";
-        for(int i = 0;i < cSize; i++){
-        	cout<<hArr[i]<<",";
-        }
-        cout<<endl;
-    }
-    int extractMin(){
-    	return extractMin(0);
-    }	
-    int extractMin(int index) 
-    { 
-        // Base case for Empty Heap
-        if(cSize == 0){
-        	cout<<"No sufficient elements\n";
-        	return 0;
-        }
-        // Base case for Heap with 1 value.
-        if(cSize == 1){
-        	cSize = 0;
-        }
-	else{
-        // Copy the last element to the root. 
-        swap(&hArr[index],&hArr[cSize-1]);
-        cSize--;
-        
-        // Call MinHeapify on root. 
-	MinHeapify(index);
-        // Return the min (Store it, before you replace the root with the last element)
-        return hArr[index];
-        }
-    }
-
-    void decreaseKey(int i, int new_val) 
-    { 
-        // Algorithm for what happens when I reduce the value of an element indexed at i to new_val. You can assume new_val to be less than the present value of the element. 
-        if(i<cSize){
-        	if(new_val<hArr[i]){
-        		hArr[i] = new_val;
-        		// Loop for correcting the Property 2.
-        		// Compare with parent and move up till i == 0 or there is no violation.
-			while (i != 0 && hArr[i]<hArr[parent(i)]){
-				swap(&hArr[i],&hArr[parent(i)]);
+		else{
+			//inserting new value;
+			hArr[cSize] = value;
+			//ensuring the rule is carried over
+			int i = cSize;
+			while(i!=0 && hArr[parent(i)]>hArr[i]){
+				swap(&hArr[parent(i)],&hArr[i]);
 				i = parent(i);
-			}	  
-        	}
-        	/*else{
-        		hArr[i] = new_val;
-        		// Loop for correcting the Property 2.
-        		// Compare with left and write and move up till left or right exceeds csize
-        		MinHeapify(i);	
-        	}
-		*/
-		
-        }
-        else{
-        	cout<<"Not possible\n";
-        }
-
-    } 
-    // Deletes the element indexed at i. 
-    void deleteKeyAt(int i) 
-    { 
-        decreaseKey(i,hArr[getMin()]);
-        // Call ExtractMin
-        extractMin(i);
-        
-    }
-
-    void MinHeapify(int i) 
-    { 
-    	int smallest;
-        // Compare with left and right
-        if(left(i),right(i)< cSize){
-		if(hArr[left(i)] < hArr[i]){
-			smallest = left(i);
+			}
+			//incresing the cSize
+			cSize++;
 		}
-		if(hArr[right(i)] < hArr[smallest]){
-			smallest = right(i);
+	}
+	
+	//**************************************************************************
+	void display(){
+		for(int i = 0; i < cSize; i++){
+			cout<<hArr[i]<<",";
 		}
-		// Replace with the smallest.
-		if(smallest != i){
-			swap(&hArr[smallest],&hArr[i]);
-			// Call MinHeapify at the replaced child. 
+		cout<<endl;
+	}
+	
+	//**************************************************************************
+	void MinHeapify(int i){
+		int smallest;
+		if(left(i),right(i)<cSize){
+			if(hArr[left(i)]<hArr[i]){
+				smallest = left(i);
+			}
+			if(hArr[right(i)]<hArr[smallest]){
+				smallest = right(i);
+			}
+			if(smallest!=i){
+				swap(&hArr[smallest],&hArr[i]);
+			}
 			MinHeapify(smallest);
 		}
-        }
-        else return;      
-    } 
+		else return;
+	}
+	
+	
+	//**************************************************************************
+	int ExtractMin(int i){
+		if(cSize == 0){
+			cout<<"No sufficient elements"<<endl;
+		}
+		else if(cSize == 1){
+			cSize = 0;
+		}
+		else{
+			swap(&hArr[i],&hArr[cSize-1]);
+			MinHeapify(i);
+			return hArr[i];
+		}
+	}
+	int ExtractMin(){
+		return ExtractMin(0);
+	}
+	//**************************************************************************
+	void decreaseKey(int i,int new_value){
+		if(i<cSize){
+			hArr[i] = new_value;
+			while(i!=0 && hArr[parent(i)]>hArr[i]){
+				swap(&hArr[parent(i)],&hArr[i]);
+				i = parent(i);
+			}
+		}
+		else cout<<"Not possible";
+	}
+	//**************************************************************************
+	void deleteAt(int i){
+		decreaseKey( i,hArr[get_min()]);
+		ExtractMin(i);
+	}
+	//**************************************************************************
 
+	
 };
-
-
 int main(){
     BinaryHeap bh1(20);
     int n;
@@ -174,12 +123,4 @@ int main(){
         bh1.insert(i);
         bh1.display();
     }
-    
-    cout<<"The new root is "<<bh1.extractMin()<<endl;
-    bh1.display();
-    cout<<"At which position the value has to be deleted :";cin>>n;
-    bh1.deleteKeyAt(n);
-    bh1.display();
-    cout << endl;
-    
 }
